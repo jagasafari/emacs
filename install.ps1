53de0923a1ca452e379634a87fe033939006eee6
mika <kaszuba.mika@gmail.com>
2017-01-16 07:20:42 +0100

    more clues how to set up emacs

--- install.ps1
+++ install.ps1
@@ -1,1 +1,4 @@
-choco install emacs
+choco install emacs64
+%% cygwin grep in cygwin base and aspell in cygwin text 
+[System.Environment]::SetEnvironmentVariable("Path",$env:Path + ";c:\cygwin\bin", [System.EnvironmentVariableTarget]::Machine)
+%% install sharpkeys
